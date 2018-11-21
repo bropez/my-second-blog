9e69f5caf04029a170ab67bdb4e3fde6b2f396d8
Matthew <matthewbropez@gmail.com>
2018-11-21 16:46:45 -0500

    My Django Girls app, first commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # title = models.CharField(max_length=200)
+    # text = models.TextField()
+    # created_date = models.DateTimeField(default=timezone.now)
+    # published_date = models.DateTimeField(blank=True, null=True)
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    workout_name = models.CharField(max_length=200)
+    set_amount = models.IntegerField()
+    rep_amount = models.IntegerField()
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.workout_name
